8a6ae8f96e337f289b00592977d2268260644bc6
sjlurton <sblurton@hotmail.co.uk>
2024-02-07 20:24:42 +0000

    Update MongoDB connection string in clouddb.js

--- db/clouddb.js
+++ db/clouddb.js
@@ -2,12 +2,21 @@
  * Setup the Database URL
  */
 
- // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
+// mongodb+srv://<username>:<password>@acme-travel.dj8e35v.mongodb.net/?retryWrites=true&w=majority
 
-const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
-const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const DB_USER = "admin";
+const DB_PASSWORD = "admin123";
+const DB_NAME = "acme-travel";
+const CLUSTER_HOST = "acme-travel.dj8e35v.mongodb.net";
 
 // Setup the DB URI
-exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
+exports.DB_URI =
+  "mongodb+srv://" +
+  DB_USER +
+  ":" +
+  DB_PASSWORD +
+  "@" +
+  CLUSTER_HOST +
+  "/" +
+  DB_NAME +
+  "?retryWrites=true&w=majority";
